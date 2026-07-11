ced552df0564417c8501d39f0418d1be76ca6049
Éloi Strée <eloistree@gmail.com>
2026-07-11 16:27:17 +0200

    Create test.gd

---
+++ script/test.gd
@@ -0,0 +1,10 @@
+extends Node
+
+"""
+var addresses = IP.resolve_hostname("my-pi.local", IP.TYPE_IPV4)
+
+if addresses.size() > 0:
+    print(addresses[0])
+else:
+    print("Could not resolve hostname")
+"""
